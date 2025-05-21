5b253a78573a872f5d3ea992f273277d706a530a
jacekpotapczuk <jacekpotapczuk@gmail.com>
2025-05-21 08:11:04 +0200

    add empty unreal project

---
+++ Source/Covenant/Covenant.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Covenant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Covenant, "Covenant" );
